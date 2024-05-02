3a3a2c7413519808a26bb2963ea827e8f27d0790
Emmanuel <emmanuelokello294@gmail.com>
2024-05-02 10:23:15 +0300

    completes lab

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+export function pointsForWord(word){
+    let points = 0
+    for(let char of word){
+        points += /[aeiou]/i.test(char) ? 1 : 2;
+    }
+    return points
+}
